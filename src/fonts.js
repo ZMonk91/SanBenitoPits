2ac5f078c18e0419de4db673ff85e0bde2e47227
Zachary Monk <ZMonk91@gmail.com>
2022-06-29 12:45:16 -0700

    Better Font Support

--- src/fonts.js
+++ src/fonts.js
@@ -3,15 +3,19 @@ import {Global} from "@emotion/react"
 const Fonts = () => (
 	<Global
 		styles={`
-        @font-face {
-            font-family: 'Ravenscroft';
-            src:
-                url('./Ravenscroft.ttf') format('truetype),
-                url('./ravenscroft-webfont.woff2') format('woff2'),
-                url('./ravenscroft-webfont.woff') format('woff');
-            font-weight: normal;
-            font-style: normal;
-        }
+            @font-face {
+                font-family: 'Ravenscroft';
+                src: url('./Ravenscroft.eot');
+                src: url('./Ravenscroft.eot?#iefix') format('embedded-opentype'),
+                    url('./Ravenscroft.woff2') format('woff2'),
+                    url('./Ravenscroft.woff') format('woff'),
+                    url('./Ravenscroft.ttf') format('truetype'),
+                    url('./Ravenscroft.svg#Ravenscroft') format('svg');
+                font-weight: normal;
+                font-style: normal;
+                font-display: swap;
+}
+
     `}
 	/>
 )
